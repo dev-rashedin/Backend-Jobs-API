b874e7ba26d439dc91f50cef5b559372c2cd34f0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-21 22:56:31 +0600

    user schema updated

--- models/User.js
+++ models/User.js
@@ -3,14 +3,25 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please add a name"]
+    required: [true, 'Please provide a name'],
+    minLength: 3,
+    maxLength: 50,
   },
   email: {
     type: String,
-    required: true
+    required: [true, 'Please provide an email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
   },
   password: {
     type: String,
-    required: true
-  }
-})
+    required: [true, 'Please add a password'],
+    minLength: 6,
+    maxLength: 12
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema)
