c50faac953b8d179259b7108f51cf5ebcdcd27d3
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-23 15:59:34 +0600

    auth controller updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,13 @@
 const { StatusCodes } = require("http-status-codes")
-const User = require("../models/User")
+const User = require("../models/User");
+const BadRequestError = require("../errors/bad-request");
 
 const register = async (req, res) => {
-  console.log(req.body)
+  console.log(name, ElementInternals, password) = req.body;
+
+  if (!name || !email || !password) { 
+    throw new BadRequestError('Please provide all values')
+  }
   
   const user = await User.create({...req.body})
   res.status(StatusCodes.CREATED).json({user})
