ed66c2d1a59733e36a52be93294eace4c91df070
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-26 16:11:17 +0600

    mongoose mddleware updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,16 +9,11 @@ const register = async (req, res) => {
    if (!username || !email || !password) {
      throw new BadRequestError('Please provide username, email and password');
    }
-  
-// const salt = await bcrypt.genSalt(10)
-//   const hashedPassword = await bcrypt.hash(password, salt)
-
-//   const tempUser = {username, email, password: hashedPassword}
-
  
   const user = await User.create({...req.body})
   res.status(StatusCodes.CREATED).json(user)
 }
+
 const login = async (req, res) => {
   res.send('login user')
 }
