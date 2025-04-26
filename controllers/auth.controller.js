2d3e28fc43038578935b9e9af40f6e65dc499541
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-26 17:31:52 +0600

    token created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,21 @@
+require('dotenv').config();
 const { StatusCodes } = require("http-status-codes")
 const User = require("../models/User");
 const BadRequestError = require("../errors/bad-request");
-const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
-  const { username, email, password } = req.body;
+  // const { username, email, password } = req.body;
 
-   if (!username || !email || !password) {
-     throw new BadRequestError('Please provide username, email and password');
-   }
+  // //  if (!username || !email || !password) {
+  // //    throw new BadRequestError('Please provide username, email and password');
+  // //  } 
  
-  const user = await User.create({...req.body})
-  res.status(StatusCodes.CREATED).json(user)
+  const user = await User.create({ ...req.body })
+  
+  const token = jwt.sign({userId: user._id, username: user.username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.username }, token})
 }
 
 const login = async (req, res) => {
