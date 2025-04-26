ac898ee042afffcb90e6018f67b14c2c261a548c
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-26 15:53:36 +0600

    mongoose middleware used

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,13 +10,13 @@ const register = async (req, res) => {
      throw new BadRequestError('Please provide username, email and password');
    }
   
+// const salt = await bcrypt.genSalt(10)
+//   const hashedPassword = await bcrypt.hash(password, salt)
 
-  const hashedPassword = await bcrypt.hash(password, 10)
-
-  const tempUser = {username, email, password: hashedPassword}
+//   const tempUser = {username, email, password: hashedPassword}
 
  
-  const user = await User.create({...tempUser})
+  const user = await User.create({...req.body})
   res.status(StatusCodes.CREATED).json(user)
 }
 const login = async (req, res) => {
