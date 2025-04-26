b63fce284c40eedd9235170a403fcb19ce63847f
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-26 14:52:08 +0600

    user created hashing the password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,23 @@
 const { StatusCodes } = require("http-status-codes")
 const User = require("../models/User");
 const BadRequestError = require("../errors/bad-request");
+const bcrypt = require("bcryptjs")
 
 const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  if (!username || !email || !password) { 
-    throw new BadRequestError('Please provide username, email and password')
-  }
+   if (!username || !email || !password) {
+     throw new BadRequestError('Please provide username, email and password');
+   }
   
-  const user = await User.create({...req.body})
-  res.status(StatusCodes.CREATED).json({user})
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const tempUser = {username, email, password: hashedPassword}
+
+ 
+  const user = await User.create({...tempUser})
+  res.status(StatusCodes.CREATED).json(user)
 }
 const login = async (req, res) => {
   res.send('login user')
