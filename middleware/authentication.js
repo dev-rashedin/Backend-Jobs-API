0f3232c9f3c2f70a3a7fafdc5e0fe88df886cba3
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-27 20:03:32 +0600

    authentication middleware created

---
+++ middleware/authentication.js
@@ -0,0 +1,17 @@
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Authentication invalid' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(payload.userId).select('-password');
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Authentication invalid' });
+  }
+};
