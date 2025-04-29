62078a0ba3690b48ea5522a17435f8540471d564
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-29 11:28:49 +0600

    updated

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,17 +1,17 @@
-const authenticationMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+// const authenticationMiddleware = async (req, res, next) => {
+//   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Authentication invalid' });
-  }
+//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//     return res.status(401).json({ message: 'Authentication invalid' });
+//   }
 
-  const token = authHeader.split(' ')[1];
+//   const token = authHeader.split(' ')[1];
 
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(payload.userId).select('-password');
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Authentication invalid' });
-  }
-};
+//   try {
+//     const payload = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = await User.findById(payload.userId).select('-password');
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: 'Authentication invalid' });
+//   }
+// };
