db4550872b3733d287582d1c64bda944ca6d92fe
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-02 00:03:13 +0600

    client side code updated

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://node-course-jobs-api.onrender.com/api/v1';
+// axios.defaults.baseURL = 'https://node-course-jobs-api.onrender.com/api/v1';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
 
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem('user');
