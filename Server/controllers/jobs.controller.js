e051711d0ebcf2140422589f617f2ca9a21e65c4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-02 21:30:52 +0600

    job created successfully

--- Server/controllers/jobs.controller.js
+++ Server/controllers/jobs.controller.js
@@ -1,4 +1,6 @@
 const Job = require('../models/Jobs.model');
+const {BadRequestError, UnauthenticatedError} = require('../errors');
+const { StatusCodes } = require('http-status-codes');
 
 const getAllJobs = async (req, res) => {
   res.send('get all jobs');
@@ -9,7 +11,15 @@ const getSingleJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user)
+  req.body.createdBy = req.user.userId
+
+  const job = await Job.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ 
+    success: true,
+    message: 'Job created successfully',
+    job
+   });
 };
 
 const updateJob = async (req, res) => {
