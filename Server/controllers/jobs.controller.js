3c33ec024e6c69ae019eecd430bd956402e96a78
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-04 17:02:20 +0600

    api created to delete a job

--- Server/controllers/jobs.controller.js
+++ Server/controllers/jobs.controller.js
@@ -85,7 +85,18 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+   const {
+     user: { userId },
+     params: { id: jobId },
+   } = req;
+  
+  const job = await Job.findOneAndRemove({ _id: jobId, createdBy: req.user.userId })
+  
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  
+  res.status(StatusCodes.OK).json({ success: true, message: 'Job deleted successfully' });
 };
 
 
