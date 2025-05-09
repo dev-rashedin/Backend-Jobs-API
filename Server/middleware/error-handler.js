ea3fb9640f9f41cb2f6de4a2d8f0b1bf7d512a4d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-09 14:14:07 +0600

    errorHandlerMiddleware updated

--- Server/middleware/error-handler.js
+++ Server/middleware/error-handler.js
@@ -4,6 +4,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   let status =  err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   let message = err.message || 'Something went wrong, try again later';
 
+  if (err.name === 'ValidationError') {
+    status = StatusCodes.BAD_REQUEST;
+    message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+  }
+
+
   if (err.code && err.code === 11000) {
     message = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
     status = StatusCodes.BAD_REQUEST
