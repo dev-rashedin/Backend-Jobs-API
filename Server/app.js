b896d8c2cd0648a001f94234610af18421219b7e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-12 19:56:29 +0600

    extra packages used to improve api security

--- Server/app.js
+++ Server/app.js
@@ -5,6 +5,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit')
+
 
 
 // error handler
@@ -18,6 +24,10 @@ const authenticateUser = require('./middleware/authentication')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter())
 
  
 // routes
