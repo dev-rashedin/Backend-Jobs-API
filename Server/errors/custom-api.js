9748db90cabc0d06a863773a8af52e629ed90106
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-05 11:45:35 +0600

    code re-factored

--- Server/errors/custom-api.js
+++
@@ -1,9 +0,0 @@
-class CustomAPIError extends Error {
-  constructor(message, statusCode = 500, details = null) {
-    super(message);
-    this.statusCode = statusCode;
-    this.details = details; 
-  }
-}
-
-module.exports = CustomAPIError;
